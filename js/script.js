d57bd3a833497efe89f5dbab8bc4e69f29fe0fe3
retop56 <retop56@gmail.com>
2022-10-13 02:16:59 -0700

    Add more game functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-const choices = ["rock", "paper", "scissors"];
-
 function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function capitalCaseSelection(selection) {
+  return selection[0].toUpperCase() + selection.slice(1).toLowerCase();
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -10,75 +13,154 @@ function playRound(playerSelection, computerSelection) {
   const tieValue = 2;
 
   function win() {
-    console.log(
-      `You win! ${capitalCaseSelection(
-        playerSelection
-      )} beats ${capitalCaseSelection(computerSelection)}`
-    );
+    updateRoundResultString(winValue, playerSelection, computerSelection);
+    updateScore(winValue);
   }
 
   function lose() {
-    console.log(
-      `You lose! ${capitalCaseSelection(
-        computerSelection
-      )} beats ${capitalCaseSelection(playerSelection)}`
-    );
-  }
-
-  function capitalCaseSelection(selection) {
-    return selection[0].toUpperCase() + selection.slice(1).toLowerCase();
+    updateRoundResultString(loseValue, playerSelection, computerSelection);
+    updateScore(loseValue);
   }
 
   if (playerSelection.toLowerCase() === computerSelection) {
-    console.log(
-      `It's a tie! Both players chose ${capitalCaseSelection(playerSelection)}`
-    );
-    return tieValue;
+    updateRoundResultString(tieValue, playerSelection, computerSelection);
+    updateScore(tieValue);
+    return;
   }
 
   if (playerSelection.toLowerCase() === "rock") {
     if (computerSelection === "paper") {
       lose();
-      return loseValue;
+      return;
     } else {
       win();
-      return winValue;
+      return;
     }
   }
 
   if (playerSelection.toLowerCase() === "paper") {
     if (computerSelection === "scissors") {
       lose();
-      return loseValue;
+      return;
     } else {
       win();
-      return winValue;
+      return;
     }
   }
 
   if (playerSelection.toLowerCase() === "scissors") {
     if (computerSelection === "rock") {
       lose();
-      return loseValue;
+      return;
     } else {
       win();
-      return winValue;
+      return;
     }
   }
 }
 
+function checkFiveRoundsCompleted() {
+  // const sum = score.reduce((partialSum, a) => partialSum + a, 0);
+  return score[0] === 5 || score[1] === 5;
+}
+
+function hideSelectionButtons() {
+  rockButton.style.display = "none";
+  paperButton.style.display = "none";
+  scissorsButton.style.display = "none";
+}
+
+function whoWonGame() {
+  return score[0] === 5;
+}
+
+function setWinnerString() {
+  if (whoWonGame()) {
+    winnerString.textContent =
+      'Congratulations! You won! Press the "New Game" button to play again!';
+  } else {
+    winnerString.textContent =
+      'Sorry, but you lost! Press the "New Game" button to play again!';
+  }
+  winnerString.style.display = "block";
+}
+
+function announceWinner() {
+  hideSelectionButtons();
+  hideRoundResultString();
+  setWinnerString();
+}
+
 function updateScore(resultValue) {
   score[resultValue] += 1;
-  console.log(
-    `The score is now:\nWins: ${score[0]}\nLosses: ${score[1]}\nTies: ${score[2]}`
-  );
+  rewriteScoreString();
+  if (checkFiveRoundsCompleted()) {
+    announceWinner();
+  }
 }
 
-function game() {
-  const score = [0, 0, 0];
+function rewriteScoreString() {
+  scoreString.textContent = `The score is:\n${score[0]} wins\n${score[1]} losses\n${score[2]} ties`;
 }
 
-game();
+function resetScore() {
+  for (let i = 0; i < score.length; i++) {
+    score[i] = 0;
+  }
+  rewriteScoreString();
+}
+
+function showSelectionButtons() {
+  rockButton.style.display = "inline-block";
+  paperButton.style.display = "inline-block";
+  scissorsButton.style.display = "inline-block";
+}
+
+function resetRoundResultString() {
+  roundResultString.textContent = "";
+}
+
+function showRoundResultString() {
+  roundResultString.style.display = "block";
+}
+
+function hideWinnerString() {
+  winnerString.style.display = "none";
+}
+
+function hideRoundResultString() {
+  roundResultString.style.display = "none";
+}
+
+function updateRoundResultString(
+  roundResult,
+  playerSelection,
+  computerSelection
+) {
+  if (roundResult === 0) {
+    roundResultString.textContent = `You win! ${capitalCaseSelection(
+      playerSelection
+    )} beats ${capitalCaseSelection(computerSelection)}`;
+  } else if (roundResult === 1) {
+    roundResultString.textContent = `You lose! ${capitalCaseSelection(
+      computerSelection
+    )} beats ${capitalCaseSelection(playerSelection)}`;
+  } else {
+    roundResultString.textContent = `It's a tie! Both players chose ${capitalCaseSelection(
+      playerSelection
+    )}`;
+  }
+}
+
+function startNewGame() {
+  resetScore();
+  showSelectionButtons();
+  resetRoundResultString();
+  showRoundResultString();
+  hideWinnerString();
+}
+
+const score = [0, 0, 0];
 
 const rockButton = document.createElement("button");
 rockButton.textContent = "Rock";
@@ -100,3 +182,22 @@ document.body.append(scissorsButton);
 scissorsButton.addEventListener("click", (e) =>
   playRound("scissors", getComputerChoice())
 );
+
+const resultsDiv = document.createElement("div");
+document.body.append(resultsDiv);
+
+const scoreString = document.createElement("h1");
+scoreString.textContent = `The score is:\n${score[0]} wins\n${score[1]} losses\n${score[2]} ties`;
+resultsDiv.append(scoreString);
+
+const roundResultString = document.createElement("p");
+resultsDiv.append(roundResultString);
+
+const winnerString = document.createElement("h1");
+resultsDiv.append(winnerString);
+winnerString.style.display = "none";
+
+const newGameButton = document.createElement("button");
+newGameButton.textContent = "New Game";
+document.body.append(newGameButton);
+newGameButton.addEventListener("click", (e) => startNewGame());
